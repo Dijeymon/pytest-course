27499c4f7d1982f2cf9eb43efdce1481b965ab96
Anton Fejer <anton.28@hotmail.com>
2021-03-30 13:17:20 -0300

    Added tests that assert logs

--- api/coronavstech/companies/tests/test_api.py
+++ api/coronavstech/companies/tests/test_api.py
@@ -1,4 +1,5 @@
 import json
+import logging
 from unittest import TestCase
 
 import pytest
@@ -17,13 +18,14 @@ class BasicCompanyAPITestCase(TestCase):
     def tearDown(self) -> None:
         pass
 
+
 class TestGetCompanies(BasicCompanyAPITestCase):
     def test_zero_companies_should_return_empty_list(self) -> None:
         response = self.client.get(self.companies_url)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(json.loads(response.content), [])
 
-    def test_one_company_exists_should_succed(self) -> None:
+    def test_one_company_exists_should_succeed(self) -> None:
         test_company = Company.objects.create(name="Amazon")
         response = self.client.get(self.companies_url)
         response_content = json.loads(response.content)[0]
@@ -31,9 +33,10 @@ class TestGetCompanies(BasicCompanyAPITestCase):
         self.assertEqual(response_content.get("name"), test_company.name)
         self.assertEqual(response_content.get("status"), "Hiring")
         self.assertEqual(response_content.get("application_link"), "")
-        self.assertEqual(response_content.get("notes"),"")
+        self.assertEqual(response_content.get("notes"), "")
 
         test_company.delete()
+
 
 class TestPostCompanies(BasicCompanyAPITestCase):
     def test_create_company_without_arguments_should_fail(self) -> None:
@@ -47,11 +50,14 @@ class TestPostCompanies(BasicCompanyAPITestCase):
         response = self.client.post(path=self.companies_url, data={"name": "Apple"})
         self.assertEqual(response.status_code, 400)
         self.assertEqual(
-            json.loads(response.content), {"name": ["company with this name already exists."]}
+            json.loads(response.content),
+            {"name": ["company with this name already exists."]},
         )
 
     def test_create_company_with_only_name_all_fields_should_be_default(self) -> None:
-        response = self.client.post(path=self.companies_url, data={"name": "test company name"})
+        response = self.client.post(
+            path=self.companies_url, data={"name": "test company name"}
+        )
         self.assertEqual(response.status_code, 201)
         response_content = json.loads(response.content)
         self.assertEqual(response_content.get("name"), "test company name")
@@ -61,7 +67,8 @@ class TestPostCompanies(BasicCompanyAPITestCase):
 
     def test_create_company_with_layoffs_status_should_succeed(self) -> None:
         response = self.client.post(
-            path=self.companies_url, data={"name": "test company name", "status": "Layoffs"}
+            path=self.companies_url,
+            data={"name": "test company name", "status": "Layoffs"},
         )
         self.assertEqual(response.status_code, 201)
         response_content = json.loads(response.content)
@@ -69,8 +76,30 @@ class TestPostCompanies(BasicCompanyAPITestCase):
 
     def test_create_company_with_wrong_status_should_fail(self) -> None:
         response = self.client.post(
-            path=self.companies_url, data={"name": "test company name", "status": "WrongStatus"}
+            path=self.companies_url,
+            data={"name": "test company name", "status": "WrongStatus"},
         )
         self.assertEqual(response.status_code, 400)
         self.assertIn("WrongStatus", str(response.content))
         self.assertIn("is not a valid choice", str(response.content))
+
+
+logger = logging.getLogger("CORONA_LOGS")
+
+
+def function_that_logs_something() -> None:
+    try:
+        raise ValueError("CoronaVirus Exception")
+    except ValueError as e:
+        logger.warning(f"I am logging {str(e)}")
+
+
+def test_logged_warning_level(caplog) -> None:
+    function_that_logs_something()
+    assert "I am logging CoronaVirus Exception" in caplog.text
+
+
+def test_logged_info_level(caplog) -> None:
+    with caplog.at_level(logging.INFO):
+        logger.info("I am logging info level")
+        assert "I am logging info level" in caplog.text
